9a1fa1f25d08faa5c4907ae6c26b24e351d9b578
zhangxiaohui <zhangxiaohui@carleagroup.com>
2025-03-27 16:46:59 +0800

    wip: xxx

--- src/components/museumItemDetail/index.vue
+++ src/components/museumItemDetail/index.vue
@@ -1,5 +1,30 @@
 <template>
 	<div class="dialog-wrapper">
+		<n-carousel show-arrow>
+			<n-image
+				:previewedImgProps="{ style: { height: '90vh' } }"
+				v-for="item in images"
+				:key="item"
+				class="image-item"
+				:src="item"
+				alt=""
+			/>
+			<template #arrow="{ prev, next }">
+				<div class="custom-arrow">
+					<button type="button" class="custom-arrow--left" @click="prev">
+						<n-icon><ArrowBack /></n-icon>
+					</button>
+					<button type="button" class="custom-arrow--right" @click="next">
+						<n-icon><ArrowForward /></n-icon>
+					</button>
+				</div>
+			</template>
+			<template #dots="{ total, currentIndex, to }">
+				<ul class="custom-dots">
+					<li v-for="index of total" :key="index" :class="{ ['is-active']: currentIndex === index - 1 }" @click="to(index - 1)" />
+				</ul>
+			</template>
+		</n-carousel>
 		<div class="image-detail-wrapper">
 			<div class="header" :isMobile="isMobile">
 				<p class="title">{{ title }}</p>
@@ -9,23 +34,13 @@
 				<p class="time">出版：{{ text4 || "无数据" }}</p>
 				<p class="time">入库：{{ text5 || "无数据" }}年</p>
 			</div>
-			<div class="text-list-wrapper mt10">
-				<n-image
-					:previewedImgProps="{ style: { height: '90vh' } }"
-					width="100%"
-					v-for="item in images"
-					:key="item"
-					class="image-item"
-					:src="item"
-					alt=""
-				/>
-			</div>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import useDevice from "@/hook/window";
+import { ArrowBack, ArrowForward } from "@vicons/ionicons5";
 
 const props = withDefaults(
 	defineProps<{
