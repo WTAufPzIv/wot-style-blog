795bb4b81a1784fb106b081c5271e3fc25e2ee09
zhangxiaohui <zhangxiaohui@carleagroup.com>
2025-03-31 16:07:59 +0800

    wip: xxx

--- src/components/bottom/index.vue
+++ src/components/bottom/index.vue
@@ -1,46 +1,97 @@
 <template>
 	<div class="bottom-wrapper">
-		<div class="left">
-			<div class="title">
-				<n-icon size="30" :component="Heart" />
-				<p>特别鸣谢</p>
-			</div>
-			<div class="left-item">
-				<img src="@/assets/images/nasalogo.png" alt="" />
-				<div class="left-text">
-					<p class="left-itemtitle">美国国家航天局</p>
-					<p class="left-itemdesc">每日天文图片API数据提供</p>
+		<div class="botttom-container" :isMobile="isMobile">
+			<div class="left">
+				<div class="title">
+					<n-icon size="30" :component="Heart" />
+					<p>特别鸣谢</p>
+				</div>
+				<div class="left-item">
+					<img src="@/assets/images/nasalogo.png" alt="" />
+					<div class="left-text">
+						<p class="left-itemtitle">美国国家航天局</p>
+						<p class="left-itemdesc">每日天文图片API数据提供</p>
+					</div>
+				</div>
+				<div class="left-item">
+					<img src="@/assets/images/harvardlogo.png" alt="" />
+					<div class="left-text">
+						<p class="left-itemtitle">哈佛大学艺术博物馆</p>
+						<p class="left-itemdesc">馆藏品API数据提供</p>
+					</div>
+				</div>
+				<div class="left-item">
+					<img src="@/assets/images/nglogo.png" alt="" />
+					<div class="left-text">
+						<p class="left-itemtitle">美国国家地理杂志中文网</p>
+						<p class="left-itemdesc">每日图片API数据提供</p>
+					</div>
+				</div>
+				<div class="left-item">
+					<img src="@/assets/images/yunabaologo.jpg" alt="" />
+					<div class="left-text">
+						<p class="left-itemtitle">腾讯元宝智能体</p>
+						<p class="left-itemdesc">英文内容AI翻译技术支持</p>
+					</div>
 				</div>
 			</div>
-			<div class="left-item">
-				<img src="@/assets/images/nasalogo.png" alt="" />
-				<div class="left-text">
-					<p class="left-itemtitle">哈佛大学艺术博物馆</p>
-					<p class="left-itemdesc">馆藏品API数据提供</p>
-				</div>
-			</div>
-			<div class="left-item">
-				<img src="@/assets/images/nasalogo.png" alt="" />
-				<div class="left-text">
-					<p class="left-itemtitle">美国国家地理杂志中文网</p>
-					<p class="left-itemdesc">每日图片API数据提供</p>
-				</div>
-			</div>
-			<div class="left-item">
-				<img src="@/assets/images/nasalogo.png" alt="" />
-				<div class="left-text">
-					<p class="left-itemtitle">腾讯元宝智能体</p>
-					<p class="left-itemdesc">英文内容AI翻译技术支持</p>
+			<div class="right">
+				<div class="right-top">
+					<div class="title">
+						<n-icon size="30" :component="LogoLinkedin" />
+						<p>友情链接</p>
+					</div>
+					<div class="right-top-list">
+						<div class="right-item">
+							<img src="@/assets/images/linghanglogo.png" alt="" />
+							<a href="https://www.wustlinghang.cn/" target="_blank">领航工作室</a>
+						</div>
+						<div class="right-item">
+							<img src="@/assets/images/link1.jpg" alt="" />
+							<a href="https://www.wuster.vip/" target="_blank">PUKARS(Hong Jiang)的博客</a>
+						</div>
+						<div class="right-item">
+							<img src="@/assets/images/link2.jpg" alt="" />
+							<a href="https://blog.csdn.net/qq_43498164" target="_blank">忘忧(ZhengYu Zhao)的博客</a>
+						</div>
+						<div class="right-item">
+							<img src="@/assets/images/link3.jpg" alt="" />
+							<a href="https://asche.top/" target="_blank">Asche(ZiJian Wu)的博客</a>
+						</div>
+						<div class="right-item">
+							<img src="@/assets/images/link4.jpg" alt="" />
+							<a href="https://blog.csdn.net/qq_40808344" target="_blank">唐流雨(JiaJun Wu)的博客</a>
+						</div>
+						<div class="right-item">
+							<img src="@/assets/images/link5.jpg" alt="" />
+							<a href="https://wustzhh.github.io/Hugo-stack/" target="_blank">zhh(Han Zhang)的博客</a>
+						</div>
+					</div>
+					<div class="right-bottom-list">
+						<div class="title">
+							<n-icon size="30" :component="LogoWechat" />
+							<p>xh2459917510</p>
+						</div>
+						<div class="title">
+							<n-icon size="30" :component="Mail" />
+							<p>auroraaksnesos@icloud.com</p>
+						</div>
+						<div class="title">
+							<n-icon size="30" :component="LogoGithub" />
+							<p><a href="https://github.com/WTAufPzIv/WTAufPzIv.github.io" target="_blank">WTAufPzIv</a></p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="center"></div>
-		<div class="right"></div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { Heart } from "@vicons/ionicons5";
+import { Heart, LogoLinkedin, LogoWechat, Mail, LogoGithub } from "@vicons/ionicons5";
+import useDevice from "@/hook/window";
+
+const { isMobile } = useDevice();
 </script>
 
 <style scoped lang="scss">
