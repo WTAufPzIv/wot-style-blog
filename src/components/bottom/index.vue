fe40afebb3f4a03cbfeb1e670550c3ced9a302eb
zhangxiaohui <zhangxiaohui@carleagroup.com>
2025-03-31 11:59:02 +0800

    wip: xxx

--- src/components/bottom/index.vue
+++ src/components/bottom/index.vue
@@ -1,7 +1,48 @@
 <template>
-	<div class="bottom-wrapper"></div>
+	<div class="bottom-wrapper">
+		<div class="left">
+			<div class="title">
+				<n-icon size="30" :component="Heart" />
+				<p>特别鸣谢</p>
+			</div>
+			<div class="left-item">
+				<img src="@/assets/images/nasalogo.png" alt="" />
+				<div class="left-text">
+					<p class="left-itemtitle">美国国家航天局</p>
+					<p class="left-itemdesc">每日天文图片API数据提供</p>
+				</div>
+			</div>
+			<div class="left-item">
+				<img src="@/assets/images/nasalogo.png" alt="" />
+				<div class="left-text">
+					<p class="left-itemtitle">哈佛大学艺术博物馆</p>
+					<p class="left-itemdesc">馆藏品API数据提供</p>
+				</div>
+			</div>
+			<div class="left-item">
+				<img src="@/assets/images/nasalogo.png" alt="" />
+				<div class="left-text">
+					<p class="left-itemtitle">美国国家地理杂志中文网</p>
+					<p class="left-itemdesc">每日图片API数据提供</p>
+				</div>
+			</div>
+			<div class="left-item">
+				<img src="@/assets/images/nasalogo.png" alt="" />
+				<div class="left-text">
+					<p class="left-itemtitle">腾讯元宝智能体</p>
+					<p class="left-itemdesc">英文内容AI翻译技术支持</p>
+				</div>
+			</div>
+		</div>
+		<div class="center"></div>
+		<div class="right"></div>
+	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Heart } from "@vicons/ionicons5";
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
--- src/components/homeScreenTwo/index.vue
+++ src/components/homeScreenTwo/index.vue
@@ -1,12 +1,13 @@
 <template>
+	<headVideo v-if="!isMobile"></headVideo>
 	<div class="home-screen-two">
-		<headVideo v-if="!isMobile"></headVideo>
 		<headerCard :style="{ marginTop: isMobile ? '24px' : '26vw' }"></headerCard>
 		<DevideTitle>最近文章</DevideTitle>
 		<RecentBlog></RecentBlog>
 		<DevideTitle>摄影图库</DevideTitle>
 		<RecentImageBlog></RecentImageBlog>
 	</div>
+	<common-bottom></common-bottom>
 </template>
 
 <script setup lang="ts">
@@ -15,6 +16,7 @@ import headerCard from "@/components/headerCard/index.vue";
 import RecentBlog from "@/components/recentBlog/index.vue";
 import headVideo from "@/components/headerVideo/index.vue";
 import RecentImageBlog from "@/components/recentImage/index.vue";
+import CommonBottom from "@/components/bottom/index.vue";
 import useDevice from "@/hook/window";
 
 const { isMobile } = useDevice();
